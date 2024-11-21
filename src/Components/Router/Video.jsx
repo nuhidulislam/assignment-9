7d29012fde92acf9e9fbe509def0de92e802ca3d
nuhidulislam <nuhidulislam@gmail.com>
2024-11-21 18:23:34 +0600

    changed title name

--- src/Components/Router/Video.jsx
+++ src/Components/Router/Video.jsx
@@ -10,7 +10,7 @@ export default function Tutorials() {
     <div className="container mx-auto p-2">
         <Navbar></Navbar>
       <h1 className="text-3xl font-bold text-center mb-4">
-        Learn Vocabularies
+        Learn Vocabularies From Videos
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
         <div className="card card-border video-responsive">
