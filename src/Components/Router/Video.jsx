52f5b106fb0036b14936163e78d40801efa857d8
nuhidulislam <nuhidulislam@gmail.com>
2024-11-21 18:27:10 +0600

    added new design

--- src/Components/Router/Video.jsx
+++ src/Components/Router/Video.jsx
@@ -9,7 +9,7 @@ export default function Tutorials() {
     
     <div className="container mx-auto p-2">
         <Navbar></Navbar>
-      <h1 className="text-3xl font-bold text-center mb-4">
+      <h1 className="text-3xl font-bold text-center mt-10 mb-4">
         Learn Vocabularies From Videos
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
